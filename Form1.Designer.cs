134ab86278d00b926553a40e829deb143de200f3
pati-son <117106632+pati-son@users.noreply.github.com>
2022-10-31 19:54:43 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Piano
+{
+    partial class Piano
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(54, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 147);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "до";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Location = new System.Drawing.Point(153, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 147);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ре";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Location = new System.Drawing.Point(252, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 147);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ми";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGray;
+            this.button4.Location = new System.Drawing.Point(351, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 147);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "фа";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightGray;
+            this.button5.Location = new System.Drawing.Point(450, 173);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 147);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "соль";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightGray;
+            this.button6.Location = new System.Drawing.Point(549, 173);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 147);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "ля";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightGray;
+            this.button7.Location = new System.Drawing.Point(648, 173);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(93, 147);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "си";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(684, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "J";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(293, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "D";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "S";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(493, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "G";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(389, 323);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "F";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(587, 323);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "H";
+            // 
+            // Piano
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Piano";
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Piano
+{
+    public partial class Piano : Form
+    {
+        public Piano()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            /*
+              var threadParameters = new System.Threading.ThreadStart(delegate { KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.S) button2_Click(button2, null); }; });
+              var thread1 = new System.Threading.Thread(threadParameters);
+              thread1.Start();
+              добавить одновременное нажатие на кнопки!!
+             
+             */
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.A) button1_Click(button1, null); };
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.S) button2_Click(button2, null); };
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.D) button3_Click(button3, null); };
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.F) button4_Click(button4, null); };
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.G) button5_Click(button5, null); };
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.H) button6_Click(button6, null); };
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.J) button7_Click(button7, null); }; 
+
+        }
+        public char[] melody = new char[100]; //массив, куда будет записана очередность кнопок
+        int i = 0; //для обращения к элементам массива
+        /*
+         * добавить запись в файл
+           добавить продолжительностьь нажатия на кнопку
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\C.wav");
+            nota.Play();
+            melody[i] = 'C';
+            i++;;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\D.wav");
+            nota.Play();
+            melody[i] = 'D';
+            i++;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\E.wav");
+            nota.Play();
+            melody[i] = 'E'; 
+            i++;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\F.wav");
+            nota.Play();
+            melody[i] = 'F';
+            i++;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\G.wav");
+            nota.Play();
+            melody[i] = 'G';
+            i++;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\A.wav");
+            nota.Play();
+            melody[i] = 'A';
+            i++;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SoundPlayer nota = new SoundPlayer(@"C:\Users\User\Projects\(3 курс) C#_C++_Assembler\kursovaya\Piano\Resources\B.wav");
+            nota.Play();
+            melody[i] = 'B';
+            i++;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
